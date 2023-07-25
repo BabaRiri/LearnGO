ca1fcc417e732324350320d81ecabd7d8283a2c9
unknown <sewardchard@gmail.com>
2023-07-25 15:43:04 +0300

    Example of the use of using a module from github in my code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module learngo
+
+go 1.20
+
+require github.com/BabaRiri/puppy v0.0.0-20230725113558-35b6b3f1ff46
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BabaRiri/puppy"
+)
+
+func main() {
+	str1 := puppy.Bark()
+	str2 := puppy.Barks()
+
+	fmt.Printf("%v %v\n", str1, str2)
+
+	//alternative way
+	fmt.Println(puppy.Barks(), puppy.Bark())
+}
