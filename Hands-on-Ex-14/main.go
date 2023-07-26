39a7bbfc0b2a6b3dccffe9fedec26d6bd7b5bd5c
unknown <sewardchard@gmail.com>
2023-07-26 15:48:16 +0300

    Variable declartions in go exercise

---
+++ Hands-on-Ex-14/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var a int = 2023
+
+const pi = 3.14
+
+func main() {
+	b := 1998
+	fmt.Printf("Constant(const) value of pi is %v\n", pi)
+	fmt.Printf("The year(var) this was coded is %v\n", a)
+	fmt.Printf("The year(short delcartion) i was born is %v\n", b)
+}
