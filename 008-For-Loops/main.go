a101637174fb99a43c10d8b43550270d32c912ba
unknown <sewardchard@gmail.com>
2023-07-27 23:32:34 +0300

    Added other types of for loops

--- 008-For-Loops/main.go
+++ 008-For-Loops/main.go
@@ -13,4 +13,38 @@ func main() {
 		fmt.Println(x)
 		x--
 	}
+
+	for {
+		fmt.Println(x)
+		x--
+		if x == 0 {
+			fmt.Println("About to break")
+			break
+		}
+		fmt.Println("Don't break")
+	}
+
+	for i := 0; i < 20; i++ {
+		if i%2 != 0 {
+			continue
+		}
+		fmt.Println("counting even numbers: ", i)
+	}
+	
+	provinces := map[string]string{
+		"EC": "Eastern Cape",
+		"FS": "Free State",
+		"GP": "Gauteng",
+		"KZN": "KwaZulu-Natal",
+		"LP": "Limpopo",
+		"MP": "Mpumalanga",
+		"NC": "Northern Cape",
+		"NW": "North West",
+		"WC": "Western Cape",
+	}
+	
+	for key, value := range provinces {
+		fmt.Printf("%s: %s\n", key, value)
+	}
+	
 }
