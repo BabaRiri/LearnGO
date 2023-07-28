f7bd99d2fefd5cca1676b160595e53a25dd8c35d
unknown <sewardchard@gmail.com>
2023-07-28 10:59:24 +0300

    Use of switch case fir flow control

---
+++ Hands-on-Ex-25/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+    x := rand.Intn(251)
+
+	fmt.Printf("x = %v\n", x)
+
+	switch {
+	case (x >= 0 && x <= 100):
+		fmt.Printf("x is between 0 and 100")
+	case (x > 100 && x <= 200):
+		fmt.Printf("x is between 101 and 200")
+	default:
+		fmt.Printf("x is not between 201 and 250")
+	}
+}
