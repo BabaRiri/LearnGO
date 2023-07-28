12256dd9bbdfcf011aada1ef5127bc3968e6616e
unknown <sewardchard@gmail.com>
2023-07-28 12:10:53 +0300

    Used logical operators with the if statement

---
+++ Hands-on-Ex-27/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	x := rand.Intn(10)
+	y := rand.Intn(10)
+
+	fmt.Printf("x = %v\ny = %v\n", x, y)
+
+	if (x < 4 && y < 4) {
+		fmt.Println("Both x and y are below 4")
+	} else if (x > 6 && y > 6) {
+		fmt.Println("Both x and y are below 4")
+	} else if ( x >= 4 && x <= 6) {
+		fmt.Println("x is between 4 and 6")
+	} else if ( y != 5) {
+		fmt.Println("y is not 5")
+	} else {
+		fmt.Println("None of the conditions were met")
+	}
+}
+
