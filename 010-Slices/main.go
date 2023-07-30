23d53d201d671e244897d76db1f181bcc5c8fdec
unknown <sewardchard@gmail.com>
2023-07-30 19:45:21 +0300

    Worked on a slice using loops, and indexes

--- 010-Slices/main.go
+++ 010-Slices/main.go
@@ -11,4 +11,20 @@ func main() {
 		"Audi",
 	}
 	fmt.Println(slice1)
+	fmt.Printf("%T\n", slice1)
+	fmt.Printf("LENGTH: %v\n\n", len(slice1))
+
+	for _, brand := range slice1 {
+		fmt.Printf("%v\n", brand)
+	}
+
+	fmt.Printf("-------------------------------\n")
+	fmt.Printf("%v\n", slice1[0])
+	fmt.Printf("%v\n", slice1[1])
+	fmt.Printf("%v\n", slice1[2])
+	fmt.Printf("-------------------------------\n")
+
+	for i := 0; i < len(slice1); i++ {
+		fmt.Printf("%v\n", slice1[i])
+	}
 }
