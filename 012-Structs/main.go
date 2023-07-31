86e2786f0ccc71b451211b2afb29e8ef74619826
unknown <sewardchard@gmail.com>
2023-07-31 17:34:39 +0300

    Created and used an anonymous struct

--- 012-Structs/main.go
+++ 012-Structs/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//Define strutc to be used
 type planet struct {
 	name     string
 	galaxy   string
@@ -16,6 +17,7 @@ type habitable struct {
 
 func main() {
 
+	//Created structs of the defined structs
 	pEarth := planet{
 		name:     "Earth",
 		galaxy:   "Milkyway",
@@ -45,12 +47,25 @@ func main() {
 	fmt.Printf("NAME: %v\tGALAXY: %v\tMOONS: %v\tDISTANCE: %v\n", pMars.name, pMars.galaxy, pMars.numMoons, pMars.distance)
 	fmt.Printf("NAME: %v\tGALAXY: %v\tMOONS: %v\tDISTANCE: %v\n\n", pPluto.name, pPluto.galaxy, pPluto.numMoons, pPluto.distance)
 
+	//created an embedded struct
 	earthHabitable := habitable{
 		planet:    pEarth,
 		habitable: true,
 	}
 
 	fmt.Println(earthHabitable)
-	fmt.Printf("PLANET: %v\tGALAXY: %v\tHABITABLE: %v\n", earthHabitable.planet.name, earthHabitable.planet.galaxy, earthHabitable.habitable)
+	fmt.Printf("PLANET: %v\tGALAXY: %v\tHABITABLE: %v\n\n", earthHabitable.planet.name, earthHabitable.planet.galaxy, earthHabitable.habitable)
+
+	//Create an anonymous struct
+	marsHabitable := struct {
+		planet
+		habitable bool
+	}{
+		planet:    pMars,
+        habitable: false,
+	}
+
+	fmt.Println(marsHabitable)
+	fmt.Printf("PLANET: %v\tGALAXY: %v\tHABITABLE: %v\n", marsHabitable.planet.name, marsHabitable.planet.galaxy, marsHabitable.habitable)
 
 }
