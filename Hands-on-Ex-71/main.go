620af2a75c4d1bf94d5612e16edbd4a2ea2106d2
unknown <sewardchard@gmail.com>
2023-08-03 19:54:58 +0300

    Created a a function that returns another function (callback)

---
+++ Hands-on-Ex-71/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(printSquare(square, 3))
+}
+
+func printSquare(f func(int) int, a int) string {
+	x := f(a)
+	return fmt.Sprintf("The number %v squared is %v", a, x)
+}
+
+func square(n int) int {
+	return n * n
+}
