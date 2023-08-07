a8d6406433489fc144f824077e51c4768c518061
unknown <sewardchard@gmail.com>
2023-08-07 10:52:32 +0300

    Learned about dereferencing in go

--- 020-Pointers/main.go
+++ 020-Pointers/main.go
@@ -6,9 +6,13 @@ func main() {
 	x := 10
 	fmt.Println("VALUE:", x)
 	fmt.Println("ADRESS:", &x)
+	fmt.Printf("&x Value: %v\t&x Type: %T\n", &x, &x)
 
-	fmt.Printf("%v\t%T\n", &x, &x)
 	s := "Rethabile"
-	fmt.Printf("%v\t%T\n", &s, &s)
+	fmt.Printf("&s Value: %v\t&s Type: %T\n", &s, &s)
 
+	y := &s
+	fmt.Printf("y Value: %v\ty Type :%T\n", y, y)
+	fmt.Printf("y Value: %v\n", y)
+	fmt.Printf("*y Value: %v\n", *y)
 }
