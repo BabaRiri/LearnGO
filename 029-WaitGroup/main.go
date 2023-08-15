a3ca902eb37477b79382e18ae584c48c9e626c98
unknown <sewardchard@gmail.com>
2023-08-15 11:43:06 +0300

    Introduction to concurrency, waitgroup, go routines and more

---
+++ 029-WaitGroup/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	fmt.Println("OS:\t", runtime.GOOS)
+	fmt.Println("ARCH:\t", runtime.GOARCH)
+	fmt.Println("CPU:\t", runtime.NumCPU())
+	fmt.Println("GoRoutines:\t", runtime.NumGoroutine())
+
+	wg.Add(1)
+	go foo()
+	bar()
+
+	fmt.Println("CPU:\t", runtime.NumCPU())
+	fmt.Println("GoRoutines:\t", runtime.NumGoroutine())
+	wg.Wait()
+
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo - ", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for  i := 0; i < 10; i++ {
+		fmt.Println("bar - ", i)
+	}
+}
