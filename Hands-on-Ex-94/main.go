2ec2e325c58bacb014977eb457c226b61595bf60
unknown <sewardchard@gmail.com>
2023-08-16 18:04:27 +0300

    Add 100 numbers to a channel and print them

---
+++ Hands-on-Ex-94/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := gen()
+	receive(c)
+	fmt.Println("THE END!")
+}
+
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
+func gen() chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 1; i <= 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	return c
+}
