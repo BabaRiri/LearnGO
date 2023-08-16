45dc9f63b5d14a6f99dc0b974f5e2375a967e396
unknown <sewardchard@gmail.com>
2023-08-16 20:24:56 +0300

    Explored error handling with type error with information

---
+++ 040-Error-Handling/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+)
+
+func divide(a, b int) (int, error) {
+    if b == 0 {
+        return 0, errors.New("cannot divide by zero")
+    }
+    return a / b, nil
+}
+
+func main() {
+    result, err := divide(10, 0)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println(result)
+}
