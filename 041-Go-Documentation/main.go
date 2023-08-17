3bad44f436427905cd2ef0c01d6b97db21f012f9
unknown <sewardchard@gmail.com>
2023-08-17 14:45:39 +0300

    Modifyed comments

--- 041-Go-Documentation/main.go
+++ 041-Go-Documentation/main.go
@@ -4,27 +4,27 @@ import (
     "fmt"
 )
 
-// factorial function takes an integer n as input and returns the factorial of n
+// Factorial function takes an integer n as input and returns the factorial of n
 func factorial(n int) int {
-    // if n is 0 or 1, return 1
+    // If n is 0 or 1, return 1
     if n == 0 || n == 1 {
         return 1
     }
-    // otherwise, return n multiplied by the factorial of n-1
+    // Otherwise, return n multiplied by the factorial of n-1
     return n * factorial(n-1)
 }
 
 func main() {
-    // define a variable to hold the input number
+    // Define a variable to hold the input number
     var num int
 
-    // prompt the user to enter a number
+    // Prompt the user to enter a number
     fmt.Print("Enter a number: ")
     fmt.Scan(&num)
 
-    // calculate the factorial of the input number
+    // Calculate the factorial of the input number
     result := factorial(num)
 
-    // print the result
+    // Print the result
     fmt.Printf("The factorial of %d is %d\n", num, result)
 }
